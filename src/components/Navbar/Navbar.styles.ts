ff87815c06e74f90cd8d7133ba0ecb6db44c50a4
Thomas <cyzmenpl@gmail.com>
2022-10-13 17:13:52 +0200

    added some styling to homepage

--- src/components/Navbar/Navbar.styles.ts
+++ src/components/Navbar/Navbar.styles.ts
@@ -1,26 +1,44 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link, NavLink } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+type LinkItemProps = {
+  isActive?: boolean | null
+}
 
 export const NavbarDesign = styled.nav`
-position: fixed;
-margin: 0 auto;
+  position: fixed;
+  margin: 0 auto;
   background-color: white;
   height: 60px;
   width: 100%;
 `
 
+export const LinkItem = styled(NavLink)`
+padding: 0.5em;
+font-size: 30px;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+
+
+  &.active {
+    color: #573788;
+  }
+`
+
 export const Wrapper = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
   margin: 0 auto;
-  max-width: 1000px;
+  max-width: 1400px;
   width: 100%;
+
 `
 
 export const BurgerIcon = styled.button`
-margin-left: 0.5em;
+  margin-left: 0.5em;
   border: none;
   background-color: transparent;
   font-size: 30px;
@@ -33,19 +51,18 @@ export const LinkWrapper = styled.div`
   align-items: center;
 `
 
-export const RightSection = styled.div`
+export const LogoWrapper = styled.div`
 display: flex;
-justify-content: space-between;
 align-items: center;
-height: 100%;
-margin-right: 2em;
+justify-content: center;
+  flex-direction: column;
 `
 
-export const LinkItem = styled(Link)`
-padding: 0.5em;
-font-size: 30px;
-  text-decoration: none;
-  color: black;
-  cursor: pointer;
-
+export const RightSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  margin-right: 2em;
 `
+
--- src/components/TextWrapper/TextWrapper.styles.tsx
+++ src/components/TextWrapper/TextWrapper.styles.tsx
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const TextWrap = styled.div`
   margin: 0 auto;
   width: 100%;
-  max-width: 1000px;
+  max-width: 1400px;
 `
